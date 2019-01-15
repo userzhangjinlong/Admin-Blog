dd9932a17777b5c8e729dbd0a21236ab9c1e4ca1
userzhangjinlong <825188432@qq.com>
2019-01-15 14:35:29 +0800

    修改線上正式域名地址

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,7 +1,7 @@
 // 配置全局接口域名
 
-const domain = 'http://api.blog.local/admin'; //本地接口域名
-// const domain = 'http://api.zhangjinlong-blog.cn/'; //线上接口域名
+// const domain = 'http://api.blog.local/admin'; //本地接口域名
+const domain = 'http://api.zhangjinlong-blog.cn/admin'; //线上接口域名
 
 export default {
 
